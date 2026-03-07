fa612e5feaf1c6cef7828fc54da67a738c61a4ed
정우현 <whtrade7290@gmail.com>
2026-03-07 13:08:16 +0900

    db동기화중

---
+++ prisma/migrations/20260307035750_add_language_columns_back/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `class_meeting` ADD COLUMN `language` ENUM('ko', 'ja') NOT NULL DEFAULT 'ko';
+
+-- AlterTable
+ALTER TABLE `column` ADD COLUMN `language` ENUM('ko', 'ja') NOT NULL DEFAULT 'ko';
